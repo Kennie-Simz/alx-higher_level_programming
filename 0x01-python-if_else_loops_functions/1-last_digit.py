e594505f04edab752ce7440a7d0567cb4449a38e
Kennie-Simz <nyongesa17@outlook.com>
2022-05-31 11:42:48 -0700

    Cap Avenger

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if (number < 0):
     lastDigit = -number % 10 * -1
 else:
     lastDigit = number % 10
-print(f"Last digit of {number} is {lastDigit}")
+print("Last digit of {:d} is {:d}".format(number, lastDigit), end=" ")
 if lastDigit > 5:
     print("and is greater than 5")
 elif lastDigit == 0:
