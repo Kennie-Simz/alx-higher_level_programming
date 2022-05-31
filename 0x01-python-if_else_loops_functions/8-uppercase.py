d6346305ac09dc71fb367b90d47e29b27be12c52
Kennie-Simz <nyongesa17@outlook.com>
2022-05-31 13:27:13 -0700

    spelling mistake checked

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        c = odr(i)
+        c = ord(i)
         if ord(i) >= 97 and ord(i) <= 122:
             c -= 32
         print("{:c}".format(c), end='')
