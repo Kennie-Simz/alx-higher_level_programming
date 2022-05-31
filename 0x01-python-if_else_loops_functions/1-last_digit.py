60736814ac80cb903ebd7cc7b38587b1970ddc90
Kennie-Simz <nyongesa17@outlook.com>
2022-05-31 11:33:52 -0700

    last avenger

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randit(-10000, 10000)
+if (number < 0):
+    lastDigit = -number % 10 * -1
+else:
+    lastDigit = number % 10
+print(f"Last digit of {number} is {lastDigit}")
+if lastDigit > 5:
+    print("and is greater than 5")
+elif lastDigit == 0:
+    print("and is 0")
+elif lastDigit < 6:
+    print("and is less than 6 and not 0")
