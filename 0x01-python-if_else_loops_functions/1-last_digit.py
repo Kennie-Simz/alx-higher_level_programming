3e111e9799aeffbf487d41124e44d6bf4d06c153
Kennie-Simz <nyongesa17@outlook.com>
2022-05-31 11:50:31 -0700

    Cap Avenger

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randit(-10000, 10000)
 if (number < 0):
-    lastDigit = -number % 10 * -1
+    lastDigit = number % -10
 else:
     lastDigit = number % 10
 print("Last digit of {:d} is {:d}".format(number, lastDigit), end=" ")
