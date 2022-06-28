390bb2301cc64b6800954dabb85704b40691646e
Kennie-Simz <nyongesa17@outlook.com>
2022-06-28 13:33:22 -0700

    padlock

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Locked Class with no class or object attribute
+"""
+
+
+class LockedClass():
+    """
+    Class has no class or object attribute
+    """
+    __slots__ = "first_name"
