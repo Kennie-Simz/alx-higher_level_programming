095fbb81ccc8772fb68f9612b8ac3247afc69fcb
Kennie-Simz <nyongesa17@outlook.com>
2022-07-04 12:22:37 -0700

    lookie

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains a function that returns
+the list of available attributes and methods
+of an object
+"""
+
+def lookup(obj):
+    """
+    function that returns the list of available
+    attributes, methods and objects
+    """
+    return (dir(obj))
